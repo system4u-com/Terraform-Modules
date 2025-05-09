5f6b50d1cc2c39baf4398ce16b5b9429c5b0b627
ladislavb <ladislav.blazek@icloud.com>
2025-05-09 13:24:39 +0200

    inline NSG rules instead of independent

--- azure/outputs.tf
+++ azure/outputs.tf
@@ -64,16 +64,16 @@ output "network_security_groups" {
   }
 }
 
-output "network_security_rules" {
-  value = {
-    for k, value in azurerm_network_security_rule.network_security_rules : k => {
-      id                          = value.id
-      name                        = value.name
-      network_security_group_name = value.network_security_group_name
-      resource_group_name         = value.resource_group_name
-    }
-  }
-}
+# output "network_security_rules" {
+#   value = {
+#     for k, value in azurerm_network_security_rule.network_security_rules : k => {
+#       id                          = value.id
+#       name                        = value.name
+#       network_security_group_name = value.network_security_group_name
+#       resource_group_name         = value.resource_group_name
+#     }
+#   }
+# }
 
 output "network_interfaces" {
   value = {
