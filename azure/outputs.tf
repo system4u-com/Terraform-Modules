dd432197d4956bbba2316b65cc08e1bdce91c7c2
ladislavb <ladislav.blazek@icloud.com>
2025-05-09 12:05:58 +0200

    Update outputs.tf

--- azure/outputs.tf
+++ azure/outputs.tf
@@ -33,8 +33,54 @@ output "virtual_networks" {
 output "subnets" {
   value = {
     for k, value in azurerm_subnet.subnets : k => {
+      id                   = value.id
+      name                 = value.name
+      resource_group_name  = value.resource_group_name
+      virtual_network_name = value.virtual_network_name
+    }
+  }
+}
+
+output "network_peerings" {
+  value = {
+    for k, value in azurerm_virtual_network_peering.peerings : k => {
+      id                        = value.id
+      name                      = value.name
+      resource_group_name       = value.resource_group_name
+      virtual_network_name      = value.virtual_network_name
+      remote_virtual_network_id = value.remote_virtual_network_id
+    }
+  }
+}
+
+output "network_security_groups" {
+  value = {
+    for k, value in azurerm_network_security_group.network_security_groups : k => {
       id                  = value.id
       name                = value.name
+      location            = value.location
+      resource_group_name = value.resource_group_name
+    }
+  }
+}
+
+output "network_security_rules" {
+  value = {
+    for k, value in azurerm_network_security_rule.network_security_rules : k => {
+      id                          = value.id
+      name                        = value.name
+      network_security_group_name = value.network_security_group_name
+      resource_group_name         = value.resource_group_name
+    }
+  }
+}
+
+output "network_interfaces" {
+  value = {
+    for k, value in azurerm_network_interface.network_interfaces : k => {
+      id                  = value.id
+      name                = value.name
+      location            = value.location
       resource_group_name = value.resource_group_name
     }
   }
