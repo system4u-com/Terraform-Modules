2160fe7ddbfc4a867fc5482920ff7f15704b6a61
ladislavb <ladislav.blazek@icloud.com>
2025-05-12 11:40:35 +0200

    location is mandatory variable for RG

--- az/main.tf
+++ az/main.tf
@@ -2,7 +2,6 @@ resource "azurerm_resource_group" "resource_groups" {
   for_each = var.resource_groups
 
   name     = each.key
-  # location = coalesce(each.value.location, var.default_location)
   location = each.value.location
   tags     = each.value.tags
 }
--- az/variables.tf
+++ az/variables.tf
@@ -1,8 +1,3 @@
-# variable "default_location" {
-#   description = "Default Location"
-#   default     = "northeurope"
-# }
-
 variable "resource_groups" {
   description = "Resource Groups"
   type = map(object({
