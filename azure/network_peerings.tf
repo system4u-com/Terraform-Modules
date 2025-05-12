b3b3f9034bcff9f7f6221d77005f58a40fd35796
ladislavb <ladislav.blazek@icloud.com>
2025-05-12 10:12:42 +0200

    cleanup

--- azure/network_peerings.tf
+++
@@ -1,9 +0,0 @@
-resource "azurerm_virtual_network_peering" "peerings" {
-  for_each = var.peerings
-
-  name                         = each.key
-  resource_group_name          = each.value.resource_group.name
-  virtual_network_name         = each.value.virtual_network.name
-  remote_virtual_network_id    = each.value.remote_virtual_network.id
-  allow_virtual_network_access = each.value.allow_virtual_network_access
-}
--- azure/resource_groups.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_resource_group" "resource_groups" {
-  for_each = var.resource_groups
-
-  name     = each.key
-  location = coalesce(each.value.location, var.default_location)
-  tags     = each.value.tags
-}
