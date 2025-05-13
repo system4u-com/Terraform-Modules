7749876bc413a29c23ba59594570223369696528
ladislavb <ladislav.blazek@icloud.com>
2025-05-13 15:03:14 +0200

    express route circuit

---
+++ az_network/express_route_circuit.tf
@@ -0,0 +1,20 @@
+resource "azurerm_express_route_circuit" "express_route_circuits" {
+  for_each = var.express_route_circuits
+
+  name                = coalesce(each.value.name, each.key)
+  location            = coalesce(each.value.location, each.value.resource_group.location) // Use the location from the resource group if not specified
+  resource_group_name = each.value.resource_group.name
+  service_provider_name = each.value.service_provider_name
+  peering_location      = each.value.peering_location
+  bandwidth_in_mbps     = each.value.bandwidth_in_mbps
+  express_route_port_id = each.value.express_route_port_id
+  bandwidth_in_gbps = each.value.bandwidth_in_gbps
+
+  sku {
+    tier   = each.value.sku.tier
+    family = each.value.sku.family
+  }
+
+  tags = each.value.tags
+
+}
