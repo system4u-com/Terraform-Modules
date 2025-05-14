0296303f3d46db9fa852e5d7d054cff5b3a41aef
ladislavb <ladislav.blazek@icloud.com>
2025-05-14 11:34:18 +0200

    Adding shared_key

--- az_network/express_route.tf
+++ az_network/express_route.tf
@@ -1,14 +1,14 @@
 resource "azurerm_express_route_circuit" "express_route_circuits" {
   for_each = var.express_route_circuits
 
-  name                = coalesce(each.value.name, each.key)
-  location            = coalesce(each.value.location, each.value.resource_group.location) // Use the location from the resource group if not specified
-  resource_group_name = each.value.resource_group.name
-  service_provider_name = each.value.service_provider_name
-  peering_location      = each.value.peering_location
-  bandwidth_in_mbps     = each.value.bandwidth_in_mbps
-  express_route_port_id = each.value.express_route_port_id
-  bandwidth_in_gbps = each.value.bandwidth_in_gbps
+  name                     = coalesce(each.value.name, each.key)
+  location                 = coalesce(each.value.location, each.value.resource_group.location) // Use the location from the resource group if not specified
+  resource_group_name      = each.value.resource_group.name
+  service_provider_name    = each.value.service_provider_name
+  peering_location         = each.value.peering_location
+  bandwidth_in_mbps        = each.value.bandwidth_in_mbps
+  express_route_port_id    = each.value.express_route_port_id
+  bandwidth_in_gbps        = each.value.bandwidth_in_gbps
   allow_classic_operations = each.value.allow_classic_operations
 
   sku {
@@ -30,4 +30,5 @@ resource "azurerm_express_route_circuit_peering" "express_route_circuit_peerings
   secondary_peer_address_prefix = each.value.secondary_peer_address_prefix
   ipv4_enabled                  = each.value.ipv4_enabled
   vlan_id                       = each.value.vlan_id
+  shared_key                    = each.value.shared_key
 }
