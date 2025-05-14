08bffd8f4f08362155202a04df5b0336cfc31e0d
ladislavb <ladislav.blazek@icloud.com>
2025-05-14 09:38:07 +0200

    ExpressRoute circuit export

--- az_network/outputs.tf
+++ az_network/outputs.tf
@@ -20,6 +20,17 @@ output "subnets" {
   }
 }
 
+output "express_route_circuits" {
+  value = {
+    for k, value in azurerm_express_route_circuit.express_route_circuits : k => {
+      id                  = value.id
+      name                = value.name
+      location            = value.location
+      resource_group_name = value.resource_group_name
+    }
+  }
+}
+
 output "network_interfaces" {
   value = {
     for k, value in azurerm_network_interface.network_interfaces : k => {
