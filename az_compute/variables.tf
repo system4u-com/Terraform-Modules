459757f27ab99a98952f89b57affcefa8b0267a9
Vojtěch Mikeška <129750036+vomikeska@users.noreply.github.com>
2025-05-19 14:05:04 +0200

    add managed disk + disk attachment.

--- az_compute/variables.tf
+++ az_compute/variables.tf
@@ -71,3 +71,18 @@ variable "windows_virtual_machines" {
   }))
   default = {}
 }
+
+variable "data_disks" {
+  description = "Map of data disks per VM"
+  type = map(object({
+    resource_group_name = string
+    location            = string
+    disks = list(object({
+      name                 = string
+      size_gb              = number
+      storage_account_type = optional(string,  "Standard_LRS")
+      caching              = optional(string,  "ReadWrite")
+    }))
+  }))
+  default = {}
+}
