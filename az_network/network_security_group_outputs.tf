83d2ab7d3ba0a0e74ae1f023429567608e6e3f56
ladislavb <ladislav.blazek@icloud.com>
2025-05-21 12:12:35 +0200

    NSGs into separate files

---
+++ az_network/network_security_group_outputs.tf
@@ -0,0 +1,20 @@
+output "network_security_groups" {
+  value = {
+    for k, value in azurerm_network_security_group.network_security_groups : k => {
+      id                  = value.id
+      name                = value.name
+      location            = value.location
+      resource_group_name = value.resource_group_name
+    }
+  }
+}
+
+output "network_security_rules" {
+  value = {
+    for k, value in local.network_security_rules_flattened : k => {
+      name                        = value.name
+      network_security_group_name = value.network_security_group_name
+      resource_group_name         = value.resource_group_name
+    }
+  }
+}
--- az_network/variables.tf
+++ az_network/variables.tf
@@ -91,39 +91,7 @@ variable "network_interfaces" {
   default = {}
 }
 
-variable "network_security_groups" {
-  description = "Network Security Groups"
-  type = map(object({
-    resource_group = object({
-      id       = string
-      name     = string
-      location = string
-    })
-    subnet_association = optional(object({
-      id = string
-    }), null)
-    nic_association = optional(object({
-      id = string
-    }), null)
-    tags = optional(map(string), {})
-    rules = optional(map(object({
-      priority                     = string
-      direction                    = string                // Inbound | Outbound
-      access                       = string                // Allow | Deny
-      protocol                     = optional(string, "*") // Tcp | Udp | Icmp | Esp | Ah | *
-      source_address_prefix        = optional(string)
-      source_address_prefixes      = optional(list(string), null)
-      source_port_range            = optional(string)
-      source_port_ranges           = optional(list(string), null)
-      destination_address_prefix   = optional(string)
-      destination_address_prefixes = optional(list(string), null)
-      destination_port_range       = optional(string)
-      destination_port_ranges      = optional(list(string))
-      description                  = optional(string)
-    })), {})
-  }))
-  default = {}
-}
+
 
 variable "network_route_tables" {
   description = "Route Tables"
