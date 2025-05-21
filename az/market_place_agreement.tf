7e9c0d577ea019478894d7788418a3cb6f9560eb
ladislavb <ladislav.blazek@icloud.com>
2025-05-21 11:58:58 +0200

    file split

--- az/market_place_agreement.tf
+++ az/market_place_agreement.tf
@@ -1,11 +1,3 @@
-resource "azurerm_resource_group" "resource_groups" {
-  for_each = var.resource_groups
-
-  name     = each.key
-  location = each.value.location
-  tags     = each.value.tags
-}
-
 resource "azurerm_marketplace_agreement" "marketplace_agreements" {
   for_each = var.marketplace_agreements
 
--- az/market_place_agreement_variables.tf
+++ az/market_place_agreement_variables.tf
@@ -1,12 +1,3 @@
-variable "resource_groups" {
-  description = "Resource Groups"
-  type = map(object({
-    location = string
-    tags     = optional(map(string), {})
-  }))
-  default = {}
-}
-
 variable "marketplace_agreements" {
   description = "Azure Marketplace Agreements"
   type = map(object({
