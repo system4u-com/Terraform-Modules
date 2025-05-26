dfc7f46af6788bd563ae91772892b165af34f53c
ladislavb <ladislav.blazek@icloud.com>
2025-05-26 21:06:17 +0200

    AVD Application groups

---
+++ avd/avd_app_group.main.tf
@@ -0,0 +1,21 @@
+# Create AVD DAG
+resource "azurerm_virtual_desktop_application_group" "avd_application_groups" {
+  for_each = var.application_groups
+
+  resource_group_name      = each.value.resource_group.name
+  location                 = coalesce(each.value.location, each.value.resource_group.location)
+  name                     = coalesce(each.value.name, each.key)
+
+  host_pool_id        = each.value.host_pool_id
+  type                = each.value.type
+  friendly_name       = each.value.friendly_name
+  description         = each.value.description
+}
+
+# Associate Workspace and DAG
+resource "azurerm_virtual_desktop_workspace_application_group_association" "avd_workspace_application_group_associations" {
+  for_each = var.workspace_application_group_associations
+
+  application_group_id = each.value.application_group_id
+  workspace_id         = each.value.workspace_id
+}
---
+++ avd/avd_app_group.variables.tf
@@ -0,0 +1,26 @@
+variable "application_groups" {
+  description = "List of AVD application groups"
+  type = map(object({
+    resource_group = object({
+      name     = string
+      location = string
+    })
+    location                 = optional(string)
+    name                     = optional(string)
+    type                     = string
+    host_pool_id = string
+    friendly_name = optional(string)
+    description   = optional(string)
+    tags         = optional(map(string), {})
+  }))
+  default = {}
+}
+
+variable "workspace_application_group_associations" {
+  description = "List of AVD workspace application group associations"
+  type = map(object({
+    application_group_id = string
+    workspace_id         = string
+  }))
+  default = {}
+}
