964caefbdbc06c18195bc1d6bd58ad5c8da62dbf
ladislavb <ladislav.blazek@icloud.com>
2025-05-26 10:34:32 +0200

    VpnGw to ERC connection

---
+++ az_network/virtual_network_gateway_connection.main.tf
@@ -0,0 +1,12 @@
+resource "azurerm_virtual_network_gateway_connection" "virtual_network_gateway_connections" {
+  for_each = var.virtual_network_gateway_connections
+
+  name                = coalesce(each.value.name, each.key)
+  location            = coalesce(each.value.location, each.value.resource_group.location) // Use the location from the resource group if not specified
+  resource_group_name = each.value.resource_group.name
+  tags                = merge(var.default_tags, each.value.tags)
+
+  type                       = each.value.type
+  virtual_network_gateway_id = each.value.virtual_network_gateway_id
+  express_route_circuit_id   = each.value.express_route_circuit_id
+}
---
+++ az_network/virtual_network_gateway_connection.variables.tf
@@ -0,0 +1,17 @@
+variable "virtual_network_gateway_connections" {
+  description = "Virtual Network Gateway Connections"
+  type = map(object({
+    resource_group = object({
+      id       = string
+      name     = string
+      location = string
+    })
+    location                   = optional(string) // Location of the gateway, if not specified, it will use the location of the resource group
+    name                       = optional(string) // Name of the gateway, if not specified, it will use the name of the resource group
+    type                       = string
+    virtual_network_gateway_id = string
+    express_route_circuit_id   = optional(string) // Optional, if not specified, it will not be used
+    tags                       = optional(map(string), {})
+  }))
+  default = {}
+}
