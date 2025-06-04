fd0da7b930b4675715ebd983951893a93d5fcc1e
Martin Šebek <sbk.martin@gmail.com>
2025-06-04 16:11:30 +0200

    Storage account monitoring

--- az_storage/storage_account.outputs.tf
+++ az_storage/storage_account.outputs.tf
@@ -9,3 +9,68 @@ output "storage_accounts" {
   }
   
 }
+
+output "storage_accounts_monitoring" {
+  value = {
+    for k, value in azurerm_monitor_diagnostic_setting.storage_accounts_monitoring : k => {
+      id                        = value.id
+      name                      = value.name
+      target_resource_id        = value.target_resource_id
+      monitoring_log_analytics_workspace_id = value.log_analytics_workspace_id
+      metrics                   = value.metric
+    }
+  }
+}
+
+output "storage_accounts_blobs_monitoring" {
+  value = {
+    for k, value in azurerm_monitor_diagnostic_setting.storage_accounts_blobs_monitoring : k => {
+      id                        = value.id
+      name                      = value.name
+      target_resource_id        = value.target_resource_id
+      monitoring_log_analytics_workspace_id = value.log_analytics_workspace_id
+      logs                      = value.enabled_log
+      metrics                   = value.metric
+    }
+  }
+}
+
+output "storage_accounts_queues_monitoring" {
+  value = {
+    for k, value in azurerm_monitor_diagnostic_setting.storage_accounts_queues_monitoring : k => {
+      id                        = value.id
+      name                      = value.name
+      target_resource_id        = value.target_resource_id
+      monitoring_log_analytics_workspace_id = value.log_analytics_workspace_id
+      logs                      = value.enabled_log
+      metrics                   = value.metric
+    }
+  }
+}
+
+output "storage_accounts_tables_monitoring" {
+  value = {
+    for k, value in azurerm_monitor_diagnostic_setting.storage_accounts_tables_monitoring : k => {
+      id                        = value.id
+      name                      = value.name
+      target_resource_id        = value.target_resource_id
+      monitoring_log_analytics_workspace_id = value.log_analytics_workspace_id
+      logs                      = value.enabled_log
+      metrics                   = value.metric
+    }
+  }
+}
+
+output "storage_accounts_files_monitoring" {
+  value = {
+    for k, value in azurerm_monitor_diagnostic_setting.storage_accounts_files_monitoring : k => {
+      id                        = value.id
+      name                      = value.name
+      target_resource_id        = value.target_resource_id
+      monitoring_log_analytics_workspace_id = value.log_analytics_workspace_id
+      logs                      = value.enabled_log
+      metrics                   = value.metric
+    }
+  }
+  
+}
--- az_storage/storage_account.variables.tf
+++ az_storage/storage_account.variables.tf
@@ -7,20 +7,55 @@ variable "storage_accounts" {
       name     = string
       location = string
     })
-    location = optional(string) // Location of the Storage Account, if not specified, it will use the location of the resource group
-    account_tier = optional(string, "Standard")
-    account_kind = optional(string, "StorageV2")
-    account_replication_type = optional(string, "LRS")
-    access_tier = optional(string, "Hot")
+    location                   = optional(string) // Location of the Storage Account, if not specified, it will use the location of the resource group
+    account_tier               = optional(string, "Standard")
+    account_kind               = optional(string, "StorageV2")
+    account_replication_type   = optional(string, "LRS")
+    access_tier                = optional(string, "Hot")
     https_traffic_only_enabled = optional(bool, true)
     network_rules = optional(object({
-      bypass = optional(list(string), ["None"]) // None | Logging | Metrics | AzureServices
-      default_action = optional(string, "Deny") // Allow | Deny
-      ip_rules = optional(list(string), [])
+      bypass         = optional(list(string), ["None"]) // None | Logging | Metrics | AzureServices
+      default_action = optional(string, "Deny")         // Allow | Deny
+      ip_rules       = optional(list(string), [])
       virtual_network_subnets = optional(list(object({
         id = string
       })), [])
     }))
     tags = optional(map(string), {})
+    monitoring = optional(object({
+        monitoring_log_analytics_workspace_id = optional(string)
+        log_category                          = optional(string)
+        log_category_group                    = optional(string)
+        metrics_enabled                       = optional(bool, true)
+        metrics                               = optional(string)
+        blob = optional(object({
+          enabled            = optional(bool, true)
+          log_category       = optional(string)
+          log_category_group = optional(string)
+          metrics_enabled    = optional(bool, true)
+          metrics            = optional(string)
+        }), {})
+        queue = optional(object({
+          enabled            = optional(bool, true)
+          log_category       = optional(string)
+          log_category_group = optional(string)
+          metrics_enabled    = optional(bool, true)
+          metrics            = optional(string)
+        }), {})
+        table = optional(object({
+          enabled            = optional(bool, true)
+          log_category       = optional(string)
+          log_category_group = optional(string)
+          metrics_enabled    = optional(bool, true)
+          metrics            = optional(string)
+        }), {})
+        file = optional(object({
+          enabled            = optional(bool, true)
+          log_category       = optional(string)
+          log_category_group = optional(string)
+          metrics_enabled    = optional(bool, true)
+          metrics            = optional(string)
+        }), {})
+      }), {})
   }))
 }
