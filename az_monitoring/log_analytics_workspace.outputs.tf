aa746e3128467705e9eceb44ed915733fb322d0f
Martin Šebek <sbk.martin@gmail.com>
2025-06-06 01:49:51 +0200

    Added monitoring to LAW

--- az_monitoring/log_analytics_workspace.outputs.tf
+++ az_monitoring/log_analytics_workspace.outputs.tf
@@ -7,3 +7,17 @@ output "log_analytics_workspaces" {
     }
   }
 }
+
+output "log_analytics_workspaces_monitoring" {
+  value = {
+    for k, value in azurerm_monitor_diagnostic_setting.log_analytics_workspaces_monitoring : k => {
+      id                        = value.id
+      name                      = value.name
+      target_resource_id        = value.target_resource_id
+      monitoring_log_analytics_workspace_id = value.log_analytics_workspace_id
+      logs                      = value.enabled_log
+      metrics                   = value.metric
+    }
+  }
+  
+}
--- az_monitoring/log_analytics_workspace.variables.tf
+++ az_monitoring/log_analytics_workspace.variables.tf
@@ -6,11 +6,18 @@ variable "log_analytics_workspaces" {
       name     = string
       location = string
     })
-    location            = optional(string) // Location of the Log Analytics Workspace, if not specified, it will use the location of the resource group
-    name                = optional(string) // Name of the Log Analytics Workspace, if not specified, it will use the key of the map
-    sku                 = optional(string, "PerGB2018")
-    retention_in_days   = optional(number, 30)
-    tags                = optional(map(string), {})
+    location          = optional(string) // Location of the Log Analytics Workspace, if not specified, it will use the location of the resource group
+    name              = optional(string) // Name of the Log Analytics Workspace, if not specified, it will use the key of the map
+    sku               = optional(string, "PerGB2018")
+    retention_in_days = optional(number, 30)
+    tags              = optional(map(string), {})
+    monitoring = optional(object({
+      monitoring_log_analytics_workspace_id = optional(string)
+      log_category                          = optional(string)
+      log_category_group                    = optional(string)
+      metrics_enabled                       = optional(bool, true)
+      metrics                               = optional(list(string), ["AllMetrics"])
+    }), {})
   }))
   default = {}
 }
