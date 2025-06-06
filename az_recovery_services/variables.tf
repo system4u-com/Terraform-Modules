bf5bb683ab80875fab4fa2f992509d14906ac525
ladislavb <ladislav.blazek@icloud.com>
2025-06-06 23:12:22 +0200

    remove diagnostic settings

--- az_recovery_services/variables.tf
+++ az_recovery_services/variables.tf
@@ -3,27 +3,3 @@ variable "default_tags" {
   type        = map(string)
   default     = {}
 }
-
-variable "default_diagnostic_setting_metrics_enabled" {
-  description = "Enable or disable monitoring for the resources"
-  type        = bool
-  default     = false
-}
-
-variable "default_diagnostic_setting_logs_enabled" {
-  description = "Enable or disable monitoring for the resources"
-  type        = bool
-  default     = false
-}
-
-variable "default_diagnostic_setting_excluded_resources" {
-  description = "List of resources to exclude from monitoring"
-  type = list(string)
-  default     = []
-}
-
-variable "default_diagnostic_setting_log_analytics_workspace_id" {
-  description = "The ID of the Log Analytics Workspace for monitoring"
-  type        = string
-  default     = ""
-}
