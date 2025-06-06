58f747f1b5bf9142e121cbf4023ce64790119b63
ladislavb <ladislav.blazek@icloud.com>
2025-06-06 13:12:04 +0200

    removed ip_configuration block

--- az_recovery_services/recovery_services_vault.variables.tf
+++ az_recovery_services/recovery_services_vault.variables.tf
@@ -12,11 +12,6 @@ variable "recovery_services_vaults" {
     storage_mode_type = optional(string, "GeoRedundant") # Options: "GeoRedundant", "LocallyRedundant", "ZoneRedundant"
     immutability      = optional(string, "Locked")       # Options: "Locked", "Unlocked", "Disabled"
     tags              = optional(map(string), {})
-    ip_configurations = map(object({
-      subnet_id                     = optional(string)
-      public_ip_address_id          = optional(string)
-      private_ip_address_allocation = optional(string, "Dynamic") // The only value is Dynamic, Static is not supported
-    }))
   }))
   default = {}
 }
