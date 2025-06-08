8a52be99e57f3d34c4844f9f3919ed2173c5eb46
Martin Šebek <sbk.martin@gmail.com>
2025-06-09 01:32:59 +0200

    Virtual Network monitoring removal

--- az_network/virtual_network.main.tf
+++ az_network/virtual_network.main.tf
@@ -9,28 +9,3 @@ resource "azurerm_virtual_network" "virtual_networks" {
   tags                = each.value.tags
 }
 
-resource "azurerm_monitor_diagnostic_setting" "virtual_networks_monitoring" {
-  for_each = var.monitoring_enabled ? (
-    length(var.monitoring_included_resources) > 0 ?
-      { for k, v in var.virtual_networks : k => v if contains(var.monitoring_included_resources, coalesce(v.name, k)) } :
-      { for k, v in var.virtual_networks : k => v if !contains(var.monitoring_excluded_resources, coalesce(v.name, k)) }
-  ) : {}
-
-  name               = "${each.key}-diagnostic-setting"
-  target_resource_id = azurerm_virtual_network.virtual_networks[each.key].id
-  log_analytics_workspace_id = coalesce(each.value.monitoring.monitoring_log_analytics_workspace_id, var.monitoring_log_analytics_workspace_id)
-
-  enabled_log {
-    category = each.value.monitoring.log_category
-    category_group = (
-      each.value.monitoring.log_category == null || each.value.monitoring.log_category == "" ?
-      coalesce(each.value.monitoring.log_category_group, "allLogs") :
-      null
-    )
-  }
-
-  metric {
-    category = coalesce(each.value.monitoring.metrics, "AllMetrics")
-    enabled = each.value.monitoring.metrics_enabled
-  }
-}
--- az_network/virtual_network.variables.tf
+++ az_network/virtual_network.variables.tf
@@ -10,13 +10,6 @@ variable "virtual_networks" {
     address_space = list(string)
     dns_servers   = optional(list(string), [])
     tags          = optional(map(string), {})
-    monitoring = optional(object({
-      monitoring_log_analytics_workspace_id = optional(string)
-      log_category               = optional(string)
-      log_category_group         = optional(string)
-      metrics_enabled            = optional(bool, true)
-      metrics                    = optional(string)
-    }), {})
   }))
   default = {}
 }
