8150e0b417066ccd96acee38d74d609f0384be8c
Martin Šebek <sbk.martin@gmail.com>
2025-06-09 01:33:50 +0200

    Removed unused variables

--- az_network/variables.tf
+++ az_network/variables.tf
@@ -3,27 +3,3 @@ variable "default_tags" {
   type        = map(string)
   default     = {}
 }
-
-variable "monitoring_log_analytics_workspace_id" {
-  description = "The ID of the Log Analytics Workspace for monitoring"
-  type        = string
-  default     = ""
-}
-
-variable "monitoring_enabled" {
-  description = "Enable or disable monitoring for the resources"
-  type        = bool
-  default     = false
-}
-
-variable "monitoring_included_resources" {
-  description = "List of resources to include in monitoring"
-  type        = list(string)
-  default     = []
-}
-
-variable "monitoring_excluded_resources" {
-  description = "List of resources to exclude from monitoring"
-  type = list(string)
-  default     = []
-}
