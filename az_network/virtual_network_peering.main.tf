1547ad6fc58cdf372fb7fbe1c7cb49c4b538209e
S4U-DV <103593315+S4U-DV@users.noreply.github.com>
2025-06-18 10:37:51 +0200

    vnet peering update

--- az_network/virtual_network_peering.main.tf
+++ az_network/virtual_network_peering.main.tf
@@ -6,4 +6,7 @@ resource "azurerm_virtual_network_peering" "virtual_network_peerings" {
   virtual_network_name         = each.value.virtual_network.name
   remote_virtual_network_id    = each.value.remote_virtual_network.id
   allow_virtual_network_access = each.value.allow_virtual_network_access
+  allow_gateway_transit        = each.value.allow_gateway_transit
+  allow_forwarded_traffic      = each.value.allow_forwarded_traffic
+  use_remote_gateways          = each.value.use_remote_gateways
 }
--- az_network/virtual_network_peering.variables.tf
+++ az_network/virtual_network_peering.variables.tf
@@ -20,6 +20,9 @@ variable "virtual_network_peerings" {
       resource_group_name = string
     })
     allow_virtual_network_access = optional(bool, true)
+    allow_gateway_transit        = optional(bool, false)
+    allow_forwarded_traffic      = optional(bool, false)
+    use_remote_gateways          = optional(bool, false)
   }))
   default = {}
 }
