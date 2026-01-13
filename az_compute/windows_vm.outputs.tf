6c11d06459d84cd0c521ab3b1d599e05c6d519c7
Martin Šebek <sbk.martin@gmail.com>
2026-01-13 14:00:13 +0100

    Updated outputs to include tags for VMs

--- az_compute/windows_vm.outputs.tf
+++ az_compute/windows_vm.outputs.tf
@@ -1,9 +1,10 @@
 output "windows_vms" {
   value = {
     for k, value in azurerm_windows_virtual_machine.windows_vms : k => {
-      id       = value.id
-      name     = value.name
-      location = value.location
+      id                    = value.id
+      name                  = value.name
+      location              = value.location
+      tags                  = value.tags
     }
   }
 }
