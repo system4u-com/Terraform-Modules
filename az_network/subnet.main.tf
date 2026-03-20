3aeb6a67f8fbe8b84960b4b8e651609d5be87ba9
S4U-DV <103593315+S4U-DV@users.noreply.github.com>
2026-03-20 14:33:21 +0100

    subnets private endpoint policies added

--- az_network/subnet.main.tf
+++ az_network/subnet.main.tf
@@ -1,6 +1,7 @@
 resource "azurerm_subnet" "subnets" {
   for_each = var.subnets
 
+<<<<<<< Updated upstream
   name                 = coalesce(each.value.name, each.key) // Use the key as the name if name not specified
   resource_group_name  = each.value.virtual_network.resource_group_name
   virtual_network_name = each.value.virtual_network.name
@@ -8,3 +9,12 @@ resource "azurerm_subnet" "subnets" {
   service_endpoints    = each.value.service_endpoints
   
 }
+=======
+  name                              = coalesce(each.value.name, each.key) // Use the key as the name if name not specified
+  resource_group_name               = each.value.virtual_network.resource_group_name
+  virtual_network_name              = each.value.virtual_network.name
+  address_prefixes                  = each.value.address_prefixes
+  service_endpoints                 = each.value.service_endpoints
+  private_endpoint_network_policies = each.value.private_endpoint_network_policies
+}
+>>>>>>> Stashed changes
--- az_network/subnet.variables.tf
+++ az_network/subnet.variables.tf
@@ -8,8 +8,14 @@ variable "subnets" {
       location            = string
       resource_group_name = string
     })
+<<<<<<< Updated upstream
     address_prefixes = list(string)
     service_endpoints = optional(list(string))
+=======
+    address_prefixes                  = list(string)
+    service_endpoints                 = optional(list(string))
+    private_endpoint_network_policies = optional(string)
+>>>>>>> Stashed changes
   }))
   default = {}
 }
